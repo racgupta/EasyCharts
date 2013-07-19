e7efb463462948f1b1bddd44616b505c692dc7fd
racgupta <racgupta@adobe.com>
2013-07-19 18:30:02 +0530

    Update styling of the XY chart
    Update styling of the XY chart to have different styles for AXIS lines
    and text

--- src/org/racgupta/EasyCharts/XYChart.java
+++ src/org/racgupta/EasyCharts/XYChart.java
@@ -9,17 +9,19 @@ package org.racgupta.EasyCharts;
  */
 public class XYChart extends Chart {
 
-		Styles axisStyle;
+		Styles lineStyle;
+		Styles textStyle;
 	
 	
 
 	public XYChart() {
 		super();
-		axisStyle = new Styles();
-		axisStyle.setFill("none");
-		axisStyle.setStroke("#000");
-		axisStyle.setFontSize("11");
-		axisStyle.setFontFamily("sans-serif");
+		lineStyle = new Styles("path.domain");
+		lineStyle.setFill("none");
+		lineStyle.setStroke("#000");
+		textStyle = new Styles();
+		textStyle.setFontSize("12");
+		textStyle.setFontFamily("sans-serif");
 		xAxis = new Axis();
 		yAxis = new Axis();
 		xAxis.setOrient("bottom");
@@ -63,16 +65,17 @@ private Boolean convertData()
 		xyChartCode += getAxisCode();
 		xyChartCode +=getTitleCode();
 		xyChartCode += "svg.append(\"g\")"+
-				".attr(\"class\",\"x axis\")"+axisStyle.getStyles()+
+				".attr(\"class\",\"x axis\")"+textStyle.getStyles()+
 				".attr(\"transform\", \"translate(0," + getHeight()+ ")\")"+
 				".call(xAxis);\n";
 		xyChartCode +="svg.append(\"text\").attr(\"x\","+(getTotalWidth()/2)+").attr(\"y\","+(getHeight()+getBottomMargin())+")"+
-				axisStyle.getStyles()+".attr(\"text-anchor\", \"middle\").text(\""+xAxis.getTitle()+"\");\n";
+				textStyle.getStyles()+".attr(\"text-anchor\", \"middle\").text(\""+xAxis.getTitle()+"\");\n";
 		xyChartCode +="svg.append(\"g\")"+
-				".attr(\"class\", \"y axis\")"+axisStyle.getStyles()+
+				".attr(\"class\", \"y axis\")"+textStyle.getStyles()+
 				".call(yAxis);\n";
 		xyChartCode +="svg.append(\"text\").attr(\"y\","+(0-getLeftMargin())+").attr(\"dy\", \"1em\").attr(\"transform\",\"rotate(-90)\")"+
-				axisStyle.getStyles()+".attr(\"x\","+(0-getHeight()/2)+").attr(\"text-anchor\", \"middle\").text(\""+yAxis.getTitle()+"\");\n";
+				textStyle.getStyles()+".attr(\"x\","+(0-getHeight()/2)+").attr(\"text-anchor\", \"middle\").text(\""+yAxis.getTitle()+"\");\n";
+		xyChartCode += lineStyle.getStyleCode();
 		return xyChartCode;
 	}
 	public void setxAxis(Axis xAxis) {
